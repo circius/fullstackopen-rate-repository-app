7a30f568de0fb515ab1f4c639a6650708d98e52d
circius <circius@posteo.de>
2020-12-31 13:47:04 +0100

    setup AuthStorage

--- App.js
+++ App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/react-hooks';
 
+import AuthStorage from './src/utils/authStorage';
+import AuthStorageContext from './src/contexts/AuthStorageContext';
 import createApolloClient from './src/utils/apolloClient';
 import Main from './src/components/Main';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => (
   <NativeRouter>
     <ApolloProvider client={apolloClient}>
-      <Main />
+      <AuthStorageContext.Provider value={authStorage}>
+        <Main />
+      </AuthStorageContext.Provider>
     </ApolloProvider>
   </NativeRouter>
 );
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -3,8 +3,20 @@ import Constants from 'expo-constants';
 
 const ENDPOINT = `${Constants.manifest.extra.apollo_uri}/graphql`;
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
   return new ApolloClient({
+    request: async (operation) => {
+      try {
+        const accessToken = await authStorage.getAccessToken();
+        operation.setContext({
+          headers: {
+            authorization: accessToken ? `Bearer ${accessToken}` : '',
+          }
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    },
     uri: ENDPOINT
   });
 };
