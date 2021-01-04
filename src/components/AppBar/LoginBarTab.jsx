cd38d2c70af397a55e8684350cb1c846789e38fc
circius <circius@posteo.de>
2021-01-04 17:54:35 +0100

    fix broken fetchPolicy

--- src/components/AppBar/LoginBarTab.jsx
+++ src/components/AppBar/LoginBarTab.jsx
@@ -8,7 +8,7 @@ import { TouchableWithoutFeedback } from 'react-native';
 
 const LoginBarTab = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  const { loading, error, data } = useQuery(AUTHORIZED_USER, { fetchPolicy: 'cache-and-network' });
+  const { loading, error, data } = useQuery(AUTHORIZED_USER);
   const [doSignOut] = useSignOut();
 
   useEffect(() => {
@@ -25,6 +25,11 @@ const LoginBarTab = () => {
     label: 'sign out',
     target: '#'
   };
+  console.log('loading:', loading);
+  console.log('data:', data);
+
+  console.log('loggedIn:', loggedIn);
+
 
   return (
     !loggedIn
