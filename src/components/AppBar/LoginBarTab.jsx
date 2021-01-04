2caa39fb85bfa0acf246b6c9aa6c242cea4b3ecb
circius <circius@posteo.de>
2021-01-04 16:37:32 +0100

    use proper effect hook not naive assignment

--- src/components/AppBar/LoginBarTab.jsx
+++ src/components/AppBar/LoginBarTab.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
 
+import { AUTHORIZED_USER } from '../../graphql/queries';
 import AppBarTab from './AppBarTab';
 
 const LoginBarTab = () => {
-  const loggedIn = false;
+  const [loggedIn, setLoggedIn] = useState(false);
+  const { loading, error, data } = useQuery(AUTHORIZED_USER, { fetchPolicy: 'cache-and-network' });
+
+  useEffect(() => {
+    if (!loading) {
+      setLoggedIn(data.authorizedUser !== null);
+    }
+  }, [loading]);
+
   const signIn = {
     label: 'sign in',
     target: '/signin'
