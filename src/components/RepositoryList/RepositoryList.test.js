5d2a85b0b196793dd0075da55e73a22431e2eb40
circius <circius@posteo.de>
2021-01-06 18:30:46 +0100

    first working tests (finally....)

---
+++ src/components/RepositoryList/RepositoryList.test.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import RepositoryListContainer from './RepositoryListContainer';
+
+describe('RepositoryList', () => {
+  describe('RepositoryListContainer', () => {
+    it('renders repository information correctly', () => {
+      const repositories = {
+        pageInfo: {
+          totalCount: 8,
+          hasNextPage: true,
+          endCursor:
+            'WyJhc3luYy1saWJyYXJ5LnJlYWN0LWFzeW5jIiwxNTg4NjU2NzUwMDc2XQ==',
+          startCursor: 'WyJqYXJlZHBhbG1lci5mb3JtaWsiLDE1ODg2NjAzNTAwNzZd',
+        },
+        edges: [
+          {
+            node: {
+              id: 'jaredpalmer.formik',
+              fullName: 'jaredpalmer/formik',
+              description: 'Build forms in React, without the tears',
+              language: 'TypeScript',
+              forksCount: 1619,
+              stargazersCount: 21856,
+              ratingAverage: 88,
+              reviewCount: 3,
+              ownerAvatarUrl:
+                'https://avatars2.githubusercontent.com/u/4060187?v=4',
+            },
+            cursor: 'WyJqYXJlZHBhbG1lci5mb3JtaWsiLDE1ODg2NjAzNTAwNzZd',
+          },
+          {
+            node: {
+              id: 'async-library.react-async',
+              fullName: 'async-library/react-async',
+              description: 'Flexible promise-based React data loader',
+              language: 'JavaScript',
+              forksCount: 69,
+              stargazersCount: 1760,
+              ratingAverage: 72,
+              ownerAvatarUrl:
+                'https://avatars1.githubusercontent.com/u/54310907?v=4',
+            },
+            cursor:
+              'WyJhc3luYy1saWJyYXJ5LnJlYWN0LWFzeW5jIiwxNTg4NjU2NzUwMDc2XQ==',
+          },
+        ],
+      };
+
+      const { getAllByTestId, getByText, debug } = render(<RepositoryListContainer repositories={repositories} />);
+      // debug();
+
+      const items = getAllByTestId('RepositoryItem');
+
+      expect(items).toHaveLength(2);
+
+      const testData = repositories.edges;
+
+
+      const edgeRenderedP = ({ node }) => {
+        const keysForStrings = ['fullName', 'description', 'language'];
+        keysForStrings.forEach(key => {
+          const testString = String(node[key]);
+          const el = getByText(testString);
+          expect(el).not.toBeUndefined();
+        });
+      };
+
+      testData.forEach(edge => edgeRenderedP(edge));
+
+
+
+    });
+  });
+});
