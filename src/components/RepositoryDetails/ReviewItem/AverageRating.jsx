999187ad29672d2e9092c6a7ff0c47c305b5147c
circius <circius@posteo.de>
2021-01-07 21:04:15 +0100

    sketch of rating bubble

--- src/components/RepositoryDetails/ReviewItem/AverageRating.jsx
+++ src/components/RepositoryDetails/ReviewItem/AverageRating.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import theme from '../../../theme';
 
 const AverageRating = ({ rating }) => {
-  return <Text>rating</Text>;
+  const styles = StyleSheet.create({
+    container: {
+      borderWidth: 3,
+      borderRadius: 100,
+      width: 50,
+      height: 50,
+      borderColor: theme.colors.primary,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    text: {
+      color: theme.colors.primary,
+      fontSize: 18
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{rating}</Text>
+    </View>
+  );
 };
 
 export default AverageRating;
