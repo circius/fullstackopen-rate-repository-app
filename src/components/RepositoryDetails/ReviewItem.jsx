593e3a4a026d12a580918c392410b9d80f5768b0
circius <circius@posteo.de>
2021-01-07 20:32:02 +0100

    template for ReviewItem

---
+++ src/components/RepositoryDetails/ReviewItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const ReviewItem = ({ item }) => {
+  return <Text>whatever</Text>
+}
+
+export default ReviewItem;
--- src/components/RepositoryDetails/ReviewList.jsx
+++ src/components/RepositoryDetails/ReviewList.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, FlatList } from 'react-native';
 
 import useReviews from '../../hooks/useReviews';
+import ItemSeparator from '../RepositoryList/ItemSeparator';
+import ReviewItem from './ReviewItem';
+
 const ReviewList = ({ id }) => {
   const { reviews } = useReviews(id);
-  console.log('reviews:', reviews);
 
-  return <Text>{id}</Text>;
+  if (!reviews) return <Text>loading...</Text>;
+
+  const { edges } = reviews;
+
+  const nodes = edges
+    ? edges.map(edge => edge.node)
+    : [];
+
+  return (
+    <FlatList
+      data={nodes}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => <ReviewItem item={item} />}
+    />
+  );
 };
 
 export default ReviewList;
