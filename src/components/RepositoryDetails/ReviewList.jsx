bed7a3fd2173f67f88bfdaa0c6a50ee312ae05df
circius <circius@posteo.de>
2021-01-07 20:25:09 +0100

    initial review getting

--- src/components/RepositoryDetails/ReviewList.jsx
+++ src/components/RepositoryDetails/ReviewList.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import useReviews from '../../hooks/useReviews';
 const ReviewList = ({ id }) => {
+  const { reviews } = useReviews(id);
+  console.log('reviews:', reviews);
+
   return <Text>{id}</Text>;
 };
 
