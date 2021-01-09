76134badbd66d0ef2451c1ac20c07b10444da2ef
circius <circius@posteo.de>
2021-01-09 15:46:26 +0100

    more-or-less-complete CreateReviewForm

--- src/components/CreateReview/CreateReviewForm.jsx
+++ src/components/CreateReview/CreateReviewForm.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import { useField } from 'formik';
+
+import FormikTextInput from '../TextInput/FormikTextInput';
+import SubmitButton from '../SubmitButton';
 
 const CreateReviewForm = () => {
-  return <Text>create review form</Text>;
+  const [repoOwnerField, repoOwnerMeta, repoOwnerHelpers] = useField('repoOwner');
+  const [repoNameField, repoNameMeta, repoNameHelpers] = useField('repoName');
+  const [ratingField, ratingMeta, ratingHelpers] = useField('rating');
+  const [reviewField, reviewMeta, reviewHelpers] = useField('review');
+
+  return (
+    <View>
+      <FormikTextInput
+        name="repoOwner"
+        placeholder="Repository owner name"
+        value={repoOwnerField.value}
+        onChangeText={text => repoOwnerHelpers.setValue(text)}
+      />
+      <FormikTextInput
+        name='repoName'
+        placeholder='Repository name'
+        value={repoNameField.value}
+        onChangeText={text => repoNameHelpers.setValue(text)}
+      />
+      <FormikTextInput
+        name='rating'
+        placeholder='Rating between 0 and 100'
+        value={ratingField.value}
+        onChangeText={text => ratingHelpers.setValue(text)}
+      />
+      <FormikTextInput
+        name='review'
+        placeholder='review between 0 and 100'
+        value={reviewField.value}
+        onChangeText={text => reviewHelpers.setValue(text)}
+      />
+      <SubmitButton label='create' />
+    </View>
+  );
 };
 
 export default CreateReviewForm;
